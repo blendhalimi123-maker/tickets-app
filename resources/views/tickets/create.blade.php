c153e0e8efe95ee24d8d41195bb8f9527a68e493
blendhalimi123-maker <blendhalimi123@gmail.com>
2025-11-25 12:03:47 -0800

    edit and create views

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -1,5 +1,69 @@
 @extends('layouts.app')
 
 @section('content')
-    <!-- Your page content goes here -->
+    @extends('layouts.app')
+
+    @section('content')
+        <h1>Create Ticket</h1>
+
+        @if ($errors->any())
+            <div style="color:red;">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form action="{{ route('tickets.store') }}" method="POST">
+            @csrf
+            <label>Title:</label><br>
+            <input type="text" name="title" value="{{ old('title') }}"><br><br>
+
+            <label>Game Date:</label><br>
+            <input type="datetime-local" name="game_date" value="{{ old('game_date') }}"><br><br>
+
+            <label>Stadium:</label><br>
+            <input type="text" name="stadium" value="{{ old('stadium') }}"><br><br>
+
+            <label>Seat Info:</label><br>
+            <input type="text" name="seat_info" value="{{ old('seat_info') }}"><br><br>
+
+            <label>Price:</label><br>
+            <input type="number" step="0.01" name="price" value="{{ old('price') }}"><br><br>
+
+            <button type="submit">Create Ticket</button>
+        </form>
+    @endsection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @endsection
--- resources/views/tickets/edit.blade.php
+++ resources/views/tickets/edit.blade.php
@@ -1,5 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-    <!-- Your page content goes here -->
+    <h1>Edit Ticket</h1>
+
+    @if ($errors->any())
+        <div style="color:red;" a>
+            <ui>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+
+            </ui>
+        </div>
+    @endif
+
+
+    <form action="{{ route('tickets.update', $ticket->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <label>Title:</label><br>
+        <input type="text" name="title" value="{{ $ticket->title }}"><br><br>
+
+        <label>Game Date:</label><br>
+        <input type="datetime-local" name="game_date"
+            value="{{ date('Y-m-d\TH:i', strtotime($ticket->game_date)) }}"><br><br>
+
+        <label>Stadium:</label><br>
+        <input type="text" name="stadium" value="{{ $ticket->stadium }}"><br><br>
+
+        <label>Seat Info:</label><br>
+        <input type="text" name="seat_info" value="{{ $ticket->seat_info }}"><br><br>
+
+        <label>Price:</label><br>
+        <input type="number" step="0.01" name="price" value="{{ $ticket->price }}"><br><br>
+
+        <label>Available:</label><br>
+        <select name="is_available">
+            <option value="1" {{ $ticket->is_available ? 'selected' : '' }}>Yes</option>
+            <option value="0" {{ !$ticket->is_available ? 'selected' : '' }}>No</option>
+        </select><br><br>
+
+        <button type="submit">Update Ticket</button>
+    </form>
 @endsection
